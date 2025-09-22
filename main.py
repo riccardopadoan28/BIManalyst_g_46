c66f60a703a51f0fd0c72e9b84beabc16226e299
riccardopadoan28 <s252396@student.dtu.dk>
2025-09-22 14:40:45 +0200

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-import ifcopenshell
+import ifcopenshell as ifc
 
-from rules import windowRule
-from rules import doorRule
+def get_column_type_names(model):
+    # Get the column type
+    columns_type=model.by_type("IfcColumnType")
+    # Warning message
+    if columns_type is None:
+        print("Warning! No IfcColumnType found in the model!")
+    else:
+        #print(columns_type) is not needed
 
-model = ifcopenshell.open("path/to/ifcfile.ifc")
+        # Define an empty dictionary to fill with column type names
+        column_type_names = []
+        for column_type in columns_type:
+            column_type_name = column_type.Name 
+            column_type_names.append(column_type_name)   
+    return column_type_names
 
-windowResult = windowRule.checkRule(model)
-doorResult = doorRule.checkRule(model)
+#def create_column_dict():
+    #int number
+    #print("How many Column Types does the report define?")
+    
 
-print("Window result:", windowResult)
-print("Door result:", doorResult)
+def main():
+    # Get the model from the path
+    model=ifc.open(r"C:\Users\ricki\Desktop\GitHub\BIManalyst_g_46\25-08-D-STR.ifc")
+    # Define the building
+    building=model.by_type("IfcBuilding")
+    # Warning message
+    if building is None:
+        print("Warning! No IfcBuilding found in the model!")
+    
+    dictionary=create_column_dict()
+
+    # Use the function to get_column_type_names and print it 
+    print(get_column_type_names(model))
+
+
+if __name__ == "__main__":
+    main()
