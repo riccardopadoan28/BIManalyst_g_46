684e1017840a34b93c6ff633e23295a0597027df
riccardopadoan28 <s252396@student.dtu.dk>
2025-09-23 14:49:06 +0200

    Final revision

--- README.md
+++ README.md
@@ -4,14 +4,16 @@ Group: 46
 
 Focus Area: Build
 
-Claims: "The client read the page 14 of the 25-08-D-STR report from folder CES_BLD_25_08 and would like to verify that the columns modeled are all described in the table."
+Claims: "The client read the page 14 of the 25-08-D-STR report from folder CES_BLD_25_08 and would like to check the profiles of the rectangular columns in the model, assuming that the material is correct."
 
 The script developed:
 1) import the ifcopenshell library;
 2) open the 25-08-D-STR.ifc file;
-3) define the building using IfcBuilding;
-4) extract all the type of columns modeled in the building;
-5) check out if there are any lack of communication between model and report: an empty dictionary is filled with column type names
+3) define a function to get the rectangular column profiles;
+4) extract all the columns modeled and fill an empty dictionary with the information grouped by profile name;
+5) call that function in the main and get the information;
+6) write the output.
 
-In the next:
-6) try to check out the profiles of the columns.
+Output:
+1) from the terminal the user can check out if there are any disalignment between profile name and dimensions;
+2) the user can check out if there are any lack of communication between the model and the report.
--- main.py
+++ main.py
@@ -1,39 +1,76 @@
+# Import ifcopenshell
 import ifcopenshell as ifc
 
-def get_column_type_names(model):
-    # Get the column type
-    columns_type=model.by_type("IfcColumnType")
-    # Warning message
-    if columns_type is None:
-        print("Warning! No IfcColumnType found in the model!")
-    else:
-        #print(columns_type) is not needed
+# Define function to get rectangle profile from the columns
+def get_rectangle_profiles_from_columns(model):
+    # Create an empty dictionary to fill with the profile
+    profiles_dictionary = {}
 
-        # Define an empty dictionary to fill with column type names
-        column_type_names = []
-        for column_type in columns_type:
-            column_type_name = column_type.Name 
-            column_type_names.append(column_type_name)   
-    return column_type_names
+    #For every column in all the IfcColumn
+    for col in model.by_type("IfcColumn"):
+        if not col.Representation: #Check if there aren't
+            continue
+        
+        # Cicle to look the IfcColumn Representations (where the geometry is stored) in Representation
+        for rep in col.Representation.Representations:
 
-#def create_column_dict():
-    #int number
-    #print("How many Column Types does the report define?")
-    
+            # For every items in the representations
+            for item in rep.Items:
 
+                # Case 1: if column has direct geometry
+                if item.is_a("IfcExtrudedAreaSolid"):
+                    # Define the profile using SweptArea
+                    profile = item.SweptArea
+                    # Check only the rectangle profile
+                    if profile.is_a("IfcRectangleProfileDef"):
+                        # Get the name of the profile
+                        name = profile.ProfileName or "Unnamed"
+                        # Look if the profile name is already in the dictionary and if not it creates an empty list
+                        if name not in profiles_dictionary:
+                            profiles_dictionary[name] = []
+                        # Append the profile in the profile name list
+                        profiles_dictionary[name].append((col.GlobalId, profile))
+
+                # Case 2: if column uses type geometry (IFC defines the geometry once)
+                elif item.is_a("IfcMappedItem"):
+                    # Get the MappedRepresentation in MappingSource (the reusable geometry definition)
+                    mapped_rep = item.MappingSource.MappedRepresentation
+                    # Search for items inside MappedRepresentation
+                    for mapped_item in mapped_rep.Items:
+                        if mapped_item.is_a("IfcExtrudedAreaSolid"):
+                            # Define the profile using SweptArea
+                            profile = mapped_item.SweptArea
+                            # Check only the rectangle profile
+                            if profile.is_a("IfcRectangleProfileDef"):
+                                # Get the name of the profile
+                                name = profile.ProfileName or "Unnamed"
+                                # Look if the profile name is already in the dictionary and if not it creates an empty list
+                                if name not in profiles_dictionary:
+                                    profiles_dictionary[name] = []
+                                # Append the profile in the profile name list
+                                profiles_dictionary[name].append((col.GlobalId, profile))
+
+    return profiles_dictionary
+
+# Define main
 def main():
-    # Get the model from the path
-    model=ifc.open(r"C:\Users\ricki\Desktop\GitHub\BIManalyst_g_46\25-08-D-STR.ifc")
-    # Define the building
-    building=model.by_type("IfcBuilding")
-    # Warning message
-    if building is None:
-        print("Warning! No IfcBuilding found in the model!")
-    
-    #dictionary=create_column_dict()
+    # Assign the file to the model variable using file path
+    model = ifc.open(r"C:\Users\ricki\Desktop\GitHub\BIManalyst_g_46\25-08-D-STR.ifc")
 
-    # Use the function to get_column_type_names and print it 
-    print(get_column_type_names(model))
+    #Call the funtion to get the profiles dictionary
+    grouped = get_rectangle_profiles_from_columns(model)
 
+    # For the items in the 
+    for profile_name, col_profiles in grouped.items():
+        print(f"Profile Name: {profile_name}")
+        #Get the information from col_profiles
+        for col_id, profile in col_profiles:
+            print(f"  Column {col_id}")
+            print(f"    XDim: {profile.XDim}")
+            print(f"    YDim: {profile.YDim}")
 
+        # To divide the profile list in the terminal
+        print("-" * 40)
+
+if __name__ == "__main__":
     main()
